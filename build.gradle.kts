ba977a0d1da12f07fa670eda2e27408443f066aa
MayaqqDev <i@mayaqq.dev>
2024-07-24 18:36:31 +0200

    fixed issue with forge

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,21 +26,22 @@ repositories {
 }
 
 dependencies {
+    if (!mcData.isForge) {
+        // Resourceful Config
+        modImplementation("com.teamresourceful.resourcefulconfig:resourcefulconfig-${mcData.loader}-${mcData.version}:${rootProject.extra["resourcefulconfig_version"]}")
 
-    // Resourceful Config
-    modImplementation("com.teamresourceful.resourcefulconfig:resourcefulconfig-${mcData.loader}-${mcData.version}:${rootProject.extra["resourcefulconfig_version"]}")
+        if (mcData.isFabric) {
+            // Base dependencies
+            modImplementation("net.fabricmc:fabric-loader:${mcData.dependencies.fabric.fabricLoaderVersion}")
+            modImplementation("net.fabricmc.fabric-api:fabric-api:${mcData.dependencies.fabric.fabricApiVersion}")
 
-    if (mcData.isFabric) {
-        // Base dependencies
-        modImplementation("net.fabricmc:fabric-loader:${mcData.dependencies.fabric.fabricLoaderVersion}")
-        modImplementation("net.fabricmc.fabric-api:fabric-api:${mcData.dependencies.fabric.fabricApiVersion}")
-
-        // Extra
-        // ModMenu
-        modImplementation("com.terraformersmc:modmenu:${rootProject.extra["modmenu_version"]}")
-    } else if (mcData.isNeoForge) {
-        // Base dependencies
-        "neoForge"("net.neoforged:neoforge:${rootProject.extra["neoforge_version"]}")
-        modImplementation("org.sinytra.forgified-fabric-api:forgified-fabric-api:${rootProject.extra["forgified_fabric_api_version"]}+${mcData.version}")
+            // Extra
+            // ModMenu
+            modImplementation("com.terraformersmc:modmenu:${rootProject.extra["modmenu_version"]}")
+        } else if (mcData.isNeoForge) {
+            // Base dependencies
+            "neoForge"("net.neoforged:neoforge:${rootProject.extra["neoforge_version"]}")
+            modImplementation("org.sinytra.forgified-fabric-api:forgified-fabric-api:${rootProject.extra["forgified_fabric_api_version"]}+${mcData.version}")
+        }
     }
 }
